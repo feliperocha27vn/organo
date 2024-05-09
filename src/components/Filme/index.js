985b95ca7bde7f46aebc70df33c6c8df596ebbef
Felipe Rocha <feliperocha.dev27@gmail.com>
2024-05-09 17:31:28 -0300

    Alterei o component Filme

--- src/components/Filme/index.js
+++ src/components/Filme/index.js
@@ -4,6 +4,7 @@ function Filme(props){
     return(
         <section className='time' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <p>Utilizamos o props para poder definir as cores de cada genero</p>
         </section>
     );
 };
